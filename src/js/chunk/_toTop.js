6a9cc8c75350aef34abc56e115d27ce7a7f42377
立山賢 <>
2022-04-24 17:55:18 +0900

    topへ戻るボタン実装

--- src/js/chunk/_toTop.js
+++
@@ -1,4 +0,0 @@
-export const toTop = {
-  init: () => {
-  }
-}
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,10 @@
 import { setHeader } from './chunk/_header'
-import { toTop } from './chunk/_toTop'
 import { setMoreButton } from './chunk/_moreButton'
 import { setScroll } from './chunk/_scroll'
 import { setAccordion } from './chunk/_accordion'
 
 window.addEventListener('DOMContentLoaded', () => {
   setHeader.init()
-  toTop.init()
   setMoreButton.init()
   setScroll.init()
   setAccordion.init()
