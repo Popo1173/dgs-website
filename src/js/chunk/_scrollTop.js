e0a3411286b55fcf36d52d124e4fa55e65a4ea52
立山賢 <>
2022-05-05 13:55:41 +0900

    scroll top表示制御

--- src/js/chunk/_scrollTop.js
+++ src/js/chunk/_scrollTop.js
@@ -3,13 +3,17 @@ export const setScrollTop = {
     setScrollTop.getCurrentHeight()
   },
   getCurrentHeight: () => {
-    $(window).scroll(function () {
+    $(window).on('scroll load', (e) => {
       const $toggle = $('.js-scroll-top')
       const $contact = $('#contact')
       const winTopY = $(window).scrollTop()
+      // id指定ページ遷移・リロードに対応
+      if (e.type === 'load') {
+        $toggle.addClass('is-hidden')
+        return
+      }
       setScrollTop.contactPositionY = $contact.offset().top
-      setScrollTop.winBottomY = winTopY + $(window).innerHeight();
-
+      setScrollTop.winBottomY = winTopY + $(window).innerHeight()
       if (winTopY === 0 || setScrollTop.contactPositionY < setScrollTop.winBottomY) {
         $toggle.addClass('is-hidden')
       } else {
